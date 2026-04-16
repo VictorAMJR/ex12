ae96dbf8ae0ab32cda992f9ec6e6d44037f1e91a
VictorAMJR <victor.middlebrooks09@gmail.com>
2026-04-16 13:41:56 -0400

    refined

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
   static boolean quit =  false;
   public static void main(String[] args) {
-    Queue myQueue = new Queue(10);
+    Queue myQueue = new Queue();
 
     myQueue.enqueue('A');
     myQueue.enqueue('B');
@@ -20,37 +20,25 @@ class Queue {
   char[] queue;
   int front;
   int size;
-  int capacity;
+  //int capacity;
 
-  public Queue(int capacity) {
-    this.capacity = capacity;
-    this.queue = new char[capacity];
+  public Queue() {
+    //this.capacity = capacity;
+    this.queue = new char[10];
     this.front = 0;
     this.size = 0;
   }
 
   public void enqueue(char element) {
-    if (size == capacity) {
-      System.out.println("Queue is full");
-      return;
-    }
-    queue[(front + size) % capacity] = element;
+    queue[(front + size)] = element;
     size++;
   }
 
   public char dequeue() {
-    if (isEmpty()) {
-      System.out.println("Queue is empty");
-      return '\u0000'; // Null character
-    }
     char item = queue[front];
-    front = (front + 1) % capacity;
+    front = (front + 1);
     size--;
     return item;
-  }
-
-  public boolean isEmpty() {
-    return size == 0;
   }
 
   public int size() {
@@ -59,7 +47,7 @@ class Queue {
 
   public void printQueue() {
     for (int i = 0; i < size; i++) {
-      System.out.print(queue[(front + i) % capacity] + " ");
+      System.out.print(queue[(front + i)] + " ");
     }
     System.out.println();
   }
